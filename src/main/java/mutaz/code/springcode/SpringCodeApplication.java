1ed01bda923fb62614cfd82c4cc151eb11ab3632
malghafary <malghafary@gmail.com>
2021-05-30 23:23:02 +0300

    7- Loading Data From External Resources

--- src/main/java/mutaz/code/springcode/SpringCodeApplication.java
+++ src/main/java/mutaz/code/springcode/SpringCodeApplication.java
@@ -11,22 +11,8 @@ import java.util.Arrays;
 public class SpringCodeApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringCodeApplication.class, args);
-        Product a = context.getBean("A", Product.class);
-        Product b = context.getBean("B", Product.class);
-        Product c = context.getBean("C", Product.class);
-        ShoppingCart cart1 = context.getBean(ShoppingCart.class);
-        cart1.addItem(a);
-        cart1.addItem(b);
-        // The default bean scope in spring is singleton , so the same
-        // shopping cart will be returned here
-        // to change this to another scope , you can use
-        // @Scope()
-        // Scopes values includes : singleton , prototype,request,session
-        ShoppingCart cart2 = context.getBean(ShoppingCart.class);
-        cart2.addItem(c);
-
-        System.out.println(cart1.getItems());
-        System.out.println(cart2.getItems());
+        Double discount = context.getBean(Double.class);
+        System.out.println(discount);
     }
 
 }
